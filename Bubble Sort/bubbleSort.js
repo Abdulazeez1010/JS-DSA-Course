b92396da2b0e7117b81816c850de9d7dabb63351
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2026-06-03 14:30:34 +0100

    solve the bubble sort challenge

---
+++ Bubble Sort/bubbleSort.js
@@ -0,0 +1,61 @@
+// MY SOLUTION
+function BubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j+1]);
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+
+// COLT'S SOLUTION
+function bubbleSort(arr) {
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; i++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    };
+
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; i++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+
+// COLT'S SOLUTION OPTIMIZATION: Optimized with noSwaps
+function bubbleSort(arr) {
+    var noSwaps;
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; i++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
