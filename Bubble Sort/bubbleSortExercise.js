c5669decb00c7d625545da52b93f8eeccd5344c3
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2026-06-03 18:45:13 +0100

    correct bubble sort exercise

--- Bubble Sort/bubbleSortExercise.js
+++ Bubble Sort/bubbleSortExercise.js
@@ -70,17 +70,15 @@
 
 function bubbleSort(arr, comparator){
   // add whatever parameters you deem necessary - good luck!
-  if (typeof comparator !== 'function') return
-  if (comparator) {
-      arr.sort(comparator)
-      return arr;
+  if (typeof comparator !== 'function') {
+    comparator = (a, b) => a - b;
   }
   
   let noSwap;
   for (let i = arr.length; i > 0; i--) {
       noSwap = true;
       for (let j = 0; j < i - 1; j++) {
-          if (arr[j] > arr[j+1]) {
+          if (comparator(arr[j], arr[j+1]) > 0) {
               [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
               noSwap = false;
           }
